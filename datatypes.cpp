af50bda2aec2bd3381f33295c25962e8360408bd
Sandeep <onlinesandeep13@gmail.com>
2022-01-13 07:58:41 +0530

    first commit

---
+++ datatypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int a;
+    cout<<"Size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"Size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"Size of char "<<sizeof(c)<<endl;
+
+    short int d;
+    cout<<"Size of short int "<<sizeof(d)<<endl;
+
+    long int e;
+    cout<<"Size of long int "<<sizeof(e)<<endl;
+
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Enter first number"<<endl;
+    int num1;
+    cin>>num1;
+
+    cout<<"Enter second number "<<endl;
+    int num2;
+    cin>>num2;
+
+    int sum;
+
+    sum = num1+num2;
+    cout<<"Sum is "<<sum<<endl;
+    
+    return 0;
+}
